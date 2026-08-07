ed96f583dadb6a3048ef7df804a7c2dd53bae564
Mimrpim <mimrpim@gmail.com>
2026-08-07 22:00:27 +0200

    update

--- scenes/main_game.gd
+++ scenes/main_game.gd
@@ -5,10 +5,16 @@ extends Node
 
 @onready var file_dialog: FileDialog = $FileDialog
 @onready var file_dialog_2: FileDialog = $FileDialog2
+@onready var tile_map_3: TileMap = $"../TileMap3"
+@onready var tile_map_4: TileMap = $"../TileMap4"
+@onready var tile_map_5: TileMap = $"../TileMap5"
 
 @onready var tilemaps = [
 	tile_map,
-	tile_map_2
+	tile_map_2,
+	tile_map_3,
+	tile_map_4,
+	tile_map_5
 ]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,6 +26,9 @@ func _ready() -> void:
 func _on_file_dialog_file_selected(path: String) -> void:
 	tile_map.map_file = path
 	tile_map_2.map_file = path.get_basename() + ".gap"
+	tile_map_3.map_file = path.get_basename() + ".ddp"
+	tile_map_4.map_file = path.get_basename() + ".dap"
+	tile_map_5.map_file = path.get_basename() + ".wap"
 	get_tree().paused = false
 	for i in tilemaps:
 		i.emit_signal("generate")
